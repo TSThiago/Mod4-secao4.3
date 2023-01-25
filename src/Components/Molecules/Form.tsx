5340dfb39fddb0b90ee54bffee41cd045ce7c131
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-01-25 11:49:50 -0300

    criação do forms e implementação da função 'POST' no botão de submit

--- src/Components/Molecules/Form.tsx
+++ src/Components/Molecules/Form.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import Input from "../Atoms/Input";
 
 export interface IProduct {
@@ -9,16 +10,54 @@ export interface IProduct {
     id: number
 }
 
-const Form : React.FC = () => {
-    return (
-        <form >
-            <Input id="nome" type="text">Nome:</Input>
-            <Input id="marca" type="text">Marca:</Input>
-            <Input id="valor" type="text">Valor:</Input>
-            <Input id="categoria" type="text">Categoria:</Input>
-            <Input id="imagem" type="text">Imagem:</Input>
-        </form>
-    )
+const Form: React.FC = () => {
+    const [formValues, setFormValues] = useState({ nome: "", marca: "", valor: undefined, categoria: "", imagem: "" });
+    const [disabled, setDisabled] = useState(true);
+
+    const registerProduct = () => {
+        let newProduct: object = formValues
+        let myInit = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        };
+        fetch('https://apigenerator.dronahq.com/api/rYKJH80w/produtoML', myInit)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+        alert(formValues.nome + " registardo com sucesso!")
+    }
+
+useEffect(() => {
+    setDisabled(!(!!formValues.nome && !!formValues.marca && !!formValues.valor && !!formValues.categoria && !!formValues.imagem));
+}, [formValues]);
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({ ...formValues, [event.target.id]: event.target.value });
+}
+
+return (
+    <form >
+        <label htmlFor="nome">Nome:</label>
+        <input onChange={handleChange} defaultValue={formValues.nome} id={"nome"} type="text" />
+        <label htmlFor="marca">Marca:</label>
+        <input onChange={handleChange} defaultValue={formValues.marca} id={"marca"} type="text" />
+        <label htmlFor="valor">Valor:</label>
+        <input onChange={handleChange} defaultValue={formValues.valor} id={"valor"} type="text" />
+        <label htmlFor="categoria">Categoria:</label>
+        <input onChange={handleChange} defaultValue={formValues.categoria} id={"categoria"} type="text" />
+        <label htmlFor="imagem">Imagem url:</label>
+        <input onChange={handleChange} defaultValue={formValues.imagem} id={"imagem"} type="text" />
+        <br></br>
+        <input onClick={registerProduct} disabled={disabled} type="submit"></input>
+    </form>
+)
 }
 
 export default Form;
